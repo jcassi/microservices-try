ab372c5c5a50587243a662bbcaa5e1b543dcc0dc
jcassi <44300147+jcassi@users.noreply.github.com>
2021-12-12 00:35:36 -0300

    add unit-prize module

---
+++ unit_prize/src/main/java/juli/unit_prize/UnitPrizeApplication.java
@@ -0,0 +1,13 @@
+package juli.unit_prize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitPrizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitPrizeApplication.class, args);
+	}
+
+}
---
+++ unit_prize/src/test/java/juli/unit_prize/UnitPrizeApplicationTests.java
@@ -0,0 +1,13 @@
+package juli.unit_prize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitPrizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
