94fe1eccf84791ca045bf754a61e98f496b4d089
jcassi <44300147+jcassi@users.noreply.github.com>
2021-12-22 20:48:04 -0300

    Connect microservices using Eureka naming service

--- total-price/src/main/java/juli/total_price/UnitPriceProxy.java
+++ total-price/src/main/java/juli/total_price/UnitPriceProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="unit-price", url="localhost:8000")
+@FeignClient(name="unit-price")
 public interface UnitPriceProxy {
     @GetMapping("unit-price/products/{product}")
     public Double getPrice(@PathVariable("product") String product);
